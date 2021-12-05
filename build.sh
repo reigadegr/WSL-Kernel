8be983b709f14d8d9283d78fade5bd138c06e8c1
NevadaCities <58358419+NevadaCities@users.noreply.github.com>
2021-12-05 16:06:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-sudo apt update
+sudo apt update && sudo apt dist-upgrade -y
 sudo apt install -y wget build-essential flex bison libssl-dev libelf-dev
 
 cd "${GITHUB_WORKSPACE}"
 
-wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.5.tar.xz > /dev/null 2>&1
-tar xvf linux-5.15.5.tar.xz > /dev/null 2>&1
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.6.tar.xz > /dev/null 2>&1
+tar xvf linux-5.15.6.tar.xz > /dev/null 2>&1
 
-cd linux-5.15.5
+cd linux-5.15.6
+
 cp ../config .config
 scripts/config --disable DEBUG_INFO
+
 CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
 make -j"$CPU_CORES";make modules -j"$CPU_CORES";make modules_install -j"$CPU_CORES";make install -j"$CPU_CORES"
 
 cd ..
 mkdir "artifact"
-mv linux-5.15.5/arch/x86/boot/bzImage artifact/
+mv linux-5.15.6/arch/x86/boot/bzImage artifact/
