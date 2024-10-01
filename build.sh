3f7def5f7e8cd1413e1af7b8d929b215473ec119
LinBeitsi <58358419+NevadaCities@users.noreply.github.com>
2024-10-01 17:04:51 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,16 +7,15 @@ sudo apt install -y wget build-essential flex bison libssl-dev libelf-dev dwarve
 
 cd "${GITHUB_WORKSPACE}"
 
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.10.11.tar.xz
-tar xvf linux-6.10.11.tar.xz
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.11.1.tar.xz
+tar xvf linux-6.11.1.tar.xz
 
-cd linux-6.10.11
+cd linux-6.11.1
 
 cp ../config .config
 
-make olddefconfig
 make -j$(nproc)
 
 cd ..
 mkdir "artifact"
-mv linux-6.10.11/arch/x86/boot/bzImage artifact/
+mv linux-6.11.1/arch/x86/boot/bzImage artifact/
